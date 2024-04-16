d761baaf21c46ed6b1b0fc60bf0bc321ce9406af
Evelyn Salie <ekobaddish@gmail.com>
2024-04-16 13:47:46 -0400

    Source now responds to voice activity.

--- src/pngtuber.c
+++ src/pngtuber.c
@@ -1,6 +1,7 @@
 #include <time.h>
 
 #include <obs-module.h>
+#include <obs-frontend-api.h>
 #include <graphics/image-file.h>
 #include <util/threading.h>
 #include <util/platform.h>
@@ -10,6 +11,9 @@
 #include <plugin-support.h>
 
 /* https://docs.obsproject.com/reference-sources */
+
+// TODO: Two double-destroys occur on shutdown, according to verbose logs. Try using refcounted objects correctly.
+// TODO: Add UNUSED_PARAMETER() calls where appropriate.
 
 /* ---- pngtuber_data ---- */
 
@@ -38,6 +42,12 @@ typedef struct pngtuber_data {
 	
 	// The currently visible PNGtuber frame.
 	gs_image_file4_t* active_img;
+	
+	// The audio source that controls the mouth movement.
+	obs_weak_source_t *audio_source;
+	
+	// Whether the user is counted as speaking currently.
+	bool is_speaking;
 } pngtuber_data;
 
 pngtuber_data* eko_pngtuber_data_create() {
@@ -51,6 +61,9 @@ pngtuber_data* eko_pngtuber_data_create() {
 	
 	ctx->active_img = ctx->open_closed_img;
 	
+	ctx->audio_source = NULL;
+	ctx->is_speaking = false;
+	
 	return ctx;
 }
 
@@ -58,6 +71,8 @@ void eko_pngtuber_data_destroy(void* data) {
 	pngtuber_data* ctx = (pngtuber_data*) data;
 	obs_log(LOG_INFO, "Destroying pngtuber_data @ %p", ctx);
 	
+	// Free image objects.
+	obs_enter_graphics();
 	if (ctx->open_open_img != NULL) {
 		gs_image_file4_free(ctx->open_open_img);
 		bfree(ctx->open_open_img);
@@ -74,9 +89,17 @@ void eko_pngtuber_data_destroy(void* data) {
 		gs_image_file4_free(ctx->closed_closed_img);
 		bfree(ctx->closed_closed_img);
 	}
+	obs_leave_graphics();
+	
+	// Release audio source.
+	if (ctx->audio_source != NULL) {
+		obs_weak_source_release(ctx->audio_source);
+	}
 	
 	// Free context
 	bfree(ctx);
+	
+	obs_log(LOG_INFO, "Destruction Complete.", ctx);
 }
 
 // Retrieves setting_name from settings and attempts to load it as an image, interpreting it as a file path.
@@ -127,15 +150,41 @@ static const char *image_filter =
 	"WebP Files (*.webp);;"
 	"All Files (*.*)";
 
-static time_t get_modified_timestamp(const char *filename) {
-	struct stat stats;
-	if (os_stat(filename, &stats) != 0)
-		return -1;
-	return stats.st_mtime;
+static const char* eko_pngtuber_get_name(void* type_data) {
+	return "Eko's PNGTuber"; // TODO: Replace w/ localized string.
 }
 
-static const char* eko_pngtuber_get_name(void* type_data) {
-	return "Eko's PNGTuber"; // TODO: Replace with localized obs_module_text()
+// Callback to ensure that the reference to audio sources are released as necessary.
+static void on_audio_source_destroy(void *data, calldata_t *call_data) {
+	UNUSED_PARAMETER(call_data);
+	pngtuber_data* ctx = data;
+
+	obs_weak_source_release(ctx->audio_source);
+	ctx->audio_source = NULL;
+}
+
+// Callback to set flag when the user is speaking.
+void on_audio_source_capture(void *data, obs_source_t *source, const struct audio_data *audio_block, bool muted) {
+	pngtuber_data* ctx = data;
+	ctx->is_speaking = false;
+	
+	if (muted) return;
+
+	float* samples = (float*) audio_block->data[0];
+	if (samples == NULL) return;
+	
+	// Compute the average of all audio samples provided and convert those values to decibels.
+	float sum = 0.0;
+	for (size_t i = 0; i < audio_block->frames; i++) {
+		float sample = samples[i];
+		sum += sample * sample;
+	}
+	
+	double audio_level = (double) obs_mul_to_db(sqrtf(sum / audio_block->frames));
+	
+	if (audio_level > -40) {
+		ctx->is_speaking = true;
+	}
 }
 
 static void eko_pngtuber_update(void* data, obs_data_t* settings) {
@@ -143,6 +192,7 @@ static void eko_pngtuber_update(void* data, obs_data_t* settings) {
 	obs_log(LOG_INFO, "Update 0x%p w/ settings: %s", ctx, obs_data_get_json(settings));
 	
 	// Initialize an image file helper for each PNGtuber image.
+	// TODO: Only reload if the filenames have changed?
 	eko_pngtuber_attempt_load("open_open", ctx->open_open_img, settings);
 	eko_pngtuber_attempt_load("open_closed", ctx->open_closed_img, settings);
 	eko_pngtuber_attempt_load("closed_open", ctx->closed_open_img, settings);
@@ -150,6 +200,46 @@ static void eko_pngtuber_update(void* data, obs_data_t* settings) {
 	
 	ctx->blink_duration = obs_data_get_double(settings, "blink_duration") / 1000;
 	ctx->blink_gap = obs_data_get_double(settings, "blink_gap");
+	
+	// Update audio source.
+	// TODO: If new source is NULL, replace settings value with ""?
+	obs_source_t* new_audio_src = obs_get_source_by_name(obs_data_get_string(settings, "audio_src"));
+	obs_source_t* current_audio_src = NULL;
+	if (ctx->audio_source != NULL) {
+		current_audio_src = obs_weak_source_get_source(ctx->audio_source);
+	}
+	
+	obs_log(LOG_INFO, "Got audio src @ 0x%p, considering replacing 0x%p", new_audio_src, current_audio_src);
+	if (new_audio_src != current_audio_src) {
+		signal_handler_t *sig_handler;
+		
+		// Destroy callbacks ensure that our weak reference never refers to an invalid audio source.
+		// Remove current audio source destroy & capture callbacks.
+		if (current_audio_src != NULL) {
+			sig_handler = obs_source_get_signal_handler(current_audio_src);
+			signal_handler_disconnect(sig_handler, "destroy", on_audio_source_destroy, ctx);
+
+			obs_source_remove_audio_capture_callback(current_audio_src, on_audio_source_capture, ctx);
+
+			obs_weak_source_release(ctx->audio_source);
+		}
+		
+		// Add new audio source destroy & capture callbacks.
+		// TODO: Do we need to check whether new_audio_src is NULL?
+		obs_log(LOG_INFO, "Setting callbacks...");
+		sig_handler = obs_source_get_signal_handler(new_audio_src);
+		signal_handler_connect(sig_handler, "destroy", on_audio_source_destroy, ctx);
+
+		obs_source_add_audio_capture_callback(new_audio_src, on_audio_source_capture, ctx);
+
+		ctx->audio_source = obs_source_get_weak_source(new_audio_src);
+	}
+	
+	// Release strong references.
+	obs_source_release(new_audio_src);
+	if (current_audio_src != NULL) {
+		obs_source_release(current_audio_src);
+	}
 }
 
 static void eko_pngtuber_get_defaults(obs_data_t* settings) {
@@ -199,8 +289,8 @@ static void eko_pngtuber_activate(void *data)
 static uint32_t eko_pngtuber_get_width(void* data) {
 	pngtuber_data* ctx = (pngtuber_data*) data;
 	
-	if (ctx->open_open_img != NULL && ctx->open_open_img->image3.image2.image.texture != NULL) {
-		return ctx->open_open_img->image3.image2.image.cx;
+	if (ctx->active_img != NULL && ctx->active_img->image3.image2.image.texture != NULL) {
+		return ctx->active_img->image3.image2.image.cx;
 	}
 	
 	return 0;
@@ -209,8 +299,8 @@ static uint32_t eko_pngtuber_get_width(void* data) {
 static uint32_t eko_pngtuber_get_height(void* data) {
 	pngtuber_data* ctx = (pngtuber_data*) data;
 	
-	if (ctx->open_open_img != NULL && ctx->open_open_img->image3.image2.image.texture != NULL) {
-		return ctx->open_open_img->image3.image2.image.cy;
+	if (ctx->active_img != NULL && ctx->active_img->image3.image2.image.texture != NULL) {
+		return ctx->active_img->image3.image2.image.cy;
 	}
 	
 	return 0;
@@ -233,10 +323,20 @@ static void eko_pngtuber_video_render(void* data, gs_effect_t *effect) {
 	}
 	
 	if (is_blinking) {
-		ctx->active_img = ctx->closed_closed_img;
+		if (ctx->is_speaking) {
+			ctx->active_img = ctx->closed_open_img;
+		}
+		else {
+			ctx->active_img = ctx->closed_closed_img;
+		}
 	}
 	else {
-		ctx->active_img = ctx->open_closed_img;
+		if (ctx->is_speaking) {
+			ctx->active_img = ctx->open_open_img;
+		}
+		else {
+			ctx->active_img = ctx->open_closed_img;
+		}
 	}
 
 	gs_image_file4_t *const img = ctx->active_img;
@@ -259,8 +359,20 @@ static void eko_pngtuber_video_render(void* data, gs_effect_t *effect) {
 	gs_enable_framebuffer_srgb(previous);
 }
 
-static obs_properties_t *eko_pngtuber_source_properties(void *data)
-{
+// Callback used for getting a list of audio sources.
+static bool enum_audio_sources(void *data, obs_source_t *source) {
+	obs_property_t *sources_list = data;
+	uint32_t flags = obs_source_get_output_flags(source);
+
+	if ((flags & OBS_SOURCE_AUDIO) != 0) {
+		const char *name = obs_source_get_name(source);
+		obs_property_list_add_string(sources_list, name, name);
+	}
+	
+	return true;
+}
+
+static obs_properties_t *eko_pngtuber_source_properties(void *data) {
 	UNUSED_PARAMETER(data);
 	obs_log(LOG_INFO, "Source Properties");
 
@@ -271,8 +383,11 @@ static obs_properties_t *eko_pngtuber_source_properties(void *data)
 	obs_properties_add_path(props, "closed_open", "Eyes Closed Mouth Open", OBS_PATH_FILE, image_filter, NULL);
 	obs_properties_add_path(props, "closed_closed", "Eyes Closed Mouth Closed", OBS_PATH_FILE, image_filter, NULL);
 	
-	obs_properties_add_float(props, "blink_duration", "Blink Duration (ms)", 10.0, 500.0, 1.0);
-	obs_properties_add_float(props, "blink_gap", "Avg Time Between Blinks (s)", 1.0, 30.0, 1.0);
+	obs_property_t* sources = obs_properties_add_list(props, "audio_src", "Audio Source", OBS_COMBO_TYPE_LIST, OBS_COMBO_FORMAT_STRING);
+	obs_enum_sources(enum_audio_sources, sources);
+	
+	obs_properties_add_float(props, "blink_duration", "Blink Duration (ms)", 10.0, 1000.0, 1);
+	obs_properties_add_float(props, "blink_gap", "Avg Time Between Blinks (s)", 1.0, 30.0, 0.1);
 	
 	return props;
 }
@@ -282,11 +397,11 @@ static enum gs_color_space eko_pngtuber_get_color_space(void* data, size_t count
 	UNUSED_PARAMETER(preferred_spaces);
 	pngtuber_data* ctx = (pngtuber_data*) data;
 	
-	return ctx->open_open_img->space;
+	return ctx->active_img->space;
 }
 
 struct obs_source_info eko_pngtuber = {
-	.id             = "Eko's PNGtuber",
+	.id             = "ekos_pngtuber",
 	.type           = OBS_SOURCE_TYPE_INPUT,
 	.output_flags   = OBS_SOURCE_VIDEO | OBS_SOURCE_SRGB,
 	.get_name       = eko_pngtuber_get_name,
